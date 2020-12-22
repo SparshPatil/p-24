86df669a4dcedaa24f36e57b5b3a303d46960850
SparshPatil <72497039+SparshPatil@users.noreply.github.com>
2020-12-22 19:18:52 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,24 @@
+class paper
+{
+constructor(x,y,r)
+{
+
+
+    this.x x;
+this.y y;
+this.ro
+this.body-Bodies.circle(this.x, this.y, this.r/2, options)
+World.add(world, this.body);
+}
+display()
+{
+var paperpos =this.body-position;
+push()
+translate (paperpos.x, paperpos.y);
+rectMode (CENTER)
+strokeWeight(3);
+fill(255,0,255)
+ellipse(0,9, this.r, this.r);
+pop()
+}
+}
